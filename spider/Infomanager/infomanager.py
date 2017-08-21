378408f2ea43bb212740e9ed2e55ae0a1b897d18
tianjialing888 <373826647@qq.com>
2017-08-21 11:05:57 +0000

    上传文件至 'spider/Infomanager'

---
+++ spider/Infomanager/infomanager.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+from Urlmanager import urlmanger
+a = urlmanger.Uulmanger()
+
+
+def del_info(name):
+    print a.del_set(name)
+
+
+def member(name):
+    print a.urls_member(name)
+
+
+def num(name):
+    print a.url_size(name)
+
+
+if __name__ == '__main__':
+    name = 'encyclopedia_base_info'
+    num(name)
